00ebb52a41951b873651a9a87391655f536a6012
michael-mckenna <mdmckenna12@gmail.com>
2018-02-01 13:22:36 -0600

    finished part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -22,6 +22,7 @@ def sum_to_n? arr, n
   if arr.empty? || arr.length == 1
     return false
   else
+    # detects if the sum of each possible unique combination of two values equals n
     return !!arr.uniq.combination(2).detect { |a, b| a + b == n }
   end
 end
@@ -29,19 +30,55 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  # regex to see if string s starts with a consonant
+  return /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # regex doesn't work for 0 so I have a manual check
+  if s == "0" 
+    return true
+  else
+    return /^[01]*(00)$/.match(s) 
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)  
+    if isbn.empty? 
+      raise ArgumentError.new("ISBN should not be empty")
+    elsif price <= 0
+      raise ArgumentError.new("Price must be greater than or equal to 0")
+    end
+
+    # Instance variables  
+    @isbn = isbn  
+    @price = price  
+  end 
+
+  def price_as_string
+    return "$" + ('%.2f' % price.round(2))
+  end
+
+  def isbn
+    return @isbn
+  end 
+
+  def isbn=(value)
+    @isbn = value
+  end
+
+  def price
+    return @price
+  end
+
+  def price=(value)
+    @price = value
+  end
 end
